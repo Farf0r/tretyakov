69be9d4ca3fb86786aa73ce03c408d3e182b4cbb
Denis <denis.tretyakov.2006@mail.ru>
2024-11-27 15:28:23 +0300

    Update 1.py

--- 12 практическая/1.py
+++ 12 практическая/1.py
@@ -10,14 +10,7 @@ c1 = json.dumps(data1)
 pprint(c1)
 user_data = json.loads(c1)
 pprint(user_data)
-info = {
-    'company': (user_data["company"]),
-    'created_at': (user_data["created_at"]),
-    'email': (user_data["email"]),
-    'id': (user_data["id"]),
-    'name': (user_data["name"]),
-    'url': (user_data["url"])
-}
+info = {'company': (user_data["company"]),'created_at': (user_data["created_at"]),'email': (user_data["email"]),'id': (user_data["id"]),'name': (user_data["name"]),'url': (user_data["url"])}
 pprint(info)
 with open('12pc.txt', 'w') as file_json:
     json.dump(info, file_json)
