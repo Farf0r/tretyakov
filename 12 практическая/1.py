29340abc015beb91b77efcb2df3130ba8626901d
Denis <denis.tretyakov.2006@mail.ru>
2024-11-27 15:29:24 +0300

    Update 1.py

--- 12 практическая/1.py
+++ 12 практическая/1.py
@@ -1,7 +1,9 @@
 import json
 import requests
 from pprint import pprint
-
+info = 0
+with open('12pc.txt', 'w') as file_json:
+    json.dump(info, file_json)
 name = "kubernetes"
 url = f"https://api.github.com/users/{name}"
 data1 = requests.get(url).json()
@@ -12,5 +14,3 @@ user_data = json.loads(c1)
 pprint(user_data)
 info = {'company': (user_data["company"]),'created_at': (user_data["created_at"]),'email': (user_data["email"]),'id': (user_data["id"]),'name': (user_data["name"]),'url': (user_data["url"])}
 pprint(info)
-with open('12pc.txt', 'w') as file_json:
-    json.dump(info, file_json)
